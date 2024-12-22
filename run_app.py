4b25f33c8455fd1b77a931fb9ad445a77307bc77
Bogdan111022 <polyanskaolena2@gmail.com>
2024-12-22 18:25:00 +0200

    lesson2

--- run_app.py
+++ run_app.py
@@ -1,12 +1,32 @@
 from flask import *
 
+from DBManager import DBManager
+
 app = Flask("Films")
-
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/films")
+def films():
+    return render_template("films.html")
+
+@app.route("/series")
+def series():
+    return render_template("series.html")
+
+@app.route("/cartoon_series")
+def cartoon_series():
+    return render_template("cartoon_series.html")
+
+@app.route("/cartoons")
+def cartoons():
+    return render_template("cartoons.html")
+
+@app.route("/main_page")
+def main_page():
+    return render_template("main_page.html")
 
 
 app.run()
